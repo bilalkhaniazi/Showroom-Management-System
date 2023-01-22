c260235033d91e109379a8e5876ad36ca5550c59
Bilal <bilalkhaniazi@hotmail.com>
2023-01-23 00:02:29 +0500

    change no.1

--- admin.php
+++ admin.php
@@ -60,7 +60,7 @@ if(isset($_POST['update_product'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>admin</title>
+   <title>Bilal Admin</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
